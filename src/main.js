361301205a337b609de59aca6f8c437c917fef92
gem-jn <jens.neubert@gematik.de>
2024-07-26 16:20:47 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-const core = require('@actions/core');
-const fetch = require('node-fetch');
+const core = require('@actions/core')
+const fetch = require('node-fetch')
 
-async function fetchAttachments(url) {
+async function fetchAttachments(url, token) {
   try {
+    
     const response = await fetch(url, {
       headers: {
-        'Authorization': `Bearer ${token}`
+        Authorization: `Bearer ${token}`
       }
-    });
+    })
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`)
     }
-    const data = await response.json();
-    console.log('Fetched Data:', data);
+    const data = await response.json()
+    console.log('Fetched Data:', data)
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error)
   }
 }
 
@@ -22,26 +23,25 @@ async function fetchAttachments(url) {
 async function run() {
   try {
     // Get the input values from the user
-    const project = core.getInput('project');
-    const space = core.getInput('space');
-    const name = core.getInput('name');
-    const token = process.env.PET_TOKEN;
+    const project = core.getInput('project')
+    const space = core.getInput('space')
+    const name = core.getInput('name')
+    const token = process.env.PET_TOKEN
 
     // Log the input values to the GitHub Actions console
-    console.log(`The project is: ${project}`);
-    console.log(`The space is: ${space}`);
-    console.log(`The name is: ${name}`);
+    console.log(`The project is: ${project}`)
+    console.log(`The space is: ${space}`)
+    console.log(`The name is: ${name}`)
 
     // URL for fetching attachments
-    const url = `https://pet-gematikde.msappproxy.net/polarion/rest/v1/projects/${project}/spaces/${space}/documents/${name}/attachments?fields%5Bdocument_attachments%5D=%40all`;
-    await fetchAttachments(url);
-
+    const url = `https://pet-gematikde.msappproxy.net/polarion/rest/v1/projects/${project}/spaces/${space}/documents/${name}/attachments?fields%5Bdocument_attachments%5D=%40all`
+    await fetchAttachments(url, token)
   } catch (error) {
     // Handle any errors that occur during the action
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
 module.exports = {
   run
-};
+}
